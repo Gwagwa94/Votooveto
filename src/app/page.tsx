5d419047f3bbf25a04a155d32650b97779e1b320
gregoirebaumann <gregoire.baumann@nrgconsulting.es>
2025-06-26 13:59:23 +0200

    move image to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import RestoList from "@/components/RestoList";
 
 export default function Home() {
   return (
-      <main>
+      <main style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        <Image src={"/img.png"} alt="logo" width={600} height={600}/>
         <RestoList />
       </main>
   );
