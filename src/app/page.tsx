44c0ec6a11c54e4d6316d3364b70679cb03c31e5
gregoirebaumann <gregoire.baumann@nrgconsulting.es>
2025-07-01 14:52:39 +0200

    sync with redis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+'use client'
+
 import Image from "next/image";
 import RestoList from "@/components/RestoList";
+// import {Button} from "@mui/material";
+// import {getVotes} from "@/app/api/votes";
 
 export default function Home() {
   return (
       <main style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
         <Image src={"/img.png"} alt="logo" width={600} height={600}/>
         <RestoList />
+          {/*<Button onClick={async () => {console.log(await getVotes('myuser'))}}/>*/}
       </main>
   );
 }
